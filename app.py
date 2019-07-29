4a9a87074329cbc9442cfe92f25cb6cf7d8eeb2c
Shane Muirhead <shane_muirhead@hotmail.co.uk>
2019-07-29 08:04:47 +0000

    added debug=False

--- app.py
+++ app.py
@@ -135,5 +135,5 @@ if __name__ == '__main__':
     app.secret_key = 'secret123'
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
 
